0c400f8e1934f3188ada1c565b4d075896cb3455
gregt114 <gregturnberg@gmail.com>
2022-07-29 11:12:54 -0400

    vvT update

--- vvT.py
+++ vvT.py
@@ -3,25 +3,26 @@ from numpy.linalg import eigh
 import matplotlib.pyplot as plt
 from numba import jit
 
-from methods.eigengame import EigenGame
+from methods.eigengame import EigenGame as parallel_EG
 from methods.utils import find_eigenvalues
-from methods.sequential_EG import EigenGame as EG2
+from methods.sequential_EG import EigenGame as sequential_EG
 
 import time
 
 
-
+# Parameters
 OMEGA = 0.2
 TOL = 1e-6 # Convergence difference for solution
 n = 40 # n needs to be around 500 for accurate eigenvalues
 
+# Domain to solve over
 x0 = -14
 xf = 14
 dx = (xf - x0) / n
 xs = np.linspace(x0, xf, n)
 
 
-
+# Laplacian + potential
 @jit(nopython=True)
 def H():
     M = np.zeros((n,n))
@@ -35,25 +36,22 @@ def H():
                 M[r][c] = (1/dx**2) + (0.5*OMEGA**2)*x**2
     return M
 
+
+# Full matrix to find eigenvalues for
 def F(V):
-    return H() + 2*V @ V.T
+    return H() + 2*V.dot(V.T)
 
 
 
 def main():
 
-    k = 40
-    rho = 1e-6 # 1e-7 works good I think
+    # EigenGame parameters
+    k = 40 # number of eigenvectors to find
+    rho = 1e-6
     alpha = 0.0005
 
 
-    # Initial guess
-    V = [
-        np.exp(-OMEGA * xs**2 / 2),
-        xs * np.exp(-OMEGA * xs**2 / 2),
-        xs**2 * np.exp(-OMEGA * xs**2 / 2)
-    ]
-    V = np.array(V).T
+    # Randomize initial guess (n x k matrix)
     V = np.random.rand(n, k)
 
 
@@ -70,10 +68,11 @@ def main():
             old = guess
             M = F(V)
 
-            #eigVecs = EigenGame(M, k, rho, alpha) # TODO: issue is that we need all eigenvectors, not just first k?
-            eigVecs = EG2(M, k, rho, alpha, mod=20, order='small') # sequential EG
-            #eigVals, eigVecs = eigh(M)
-            #eigVecs = eigVecs[:, 0:k]
+            # TODO: is issue that we need all eigenvectors, not just first k?
+            #eigVecs = parallel_EG(M, k, rho, alpha) # parallel EigenGame
+            eigVecs = sequential_EG(M, k, rho, alpha, mod=20, order='small') # sequential EigenGame (tends to work better than parallel here)
+
+            #eigVals, eigVecs = eigh(M) # solve with regular eigenvalue solver
 
 
             guess = eigVecs.T[state]
